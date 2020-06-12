db19eebfc6e10789e786b54713b6685a0f0c409b
AvianaLilian <maxime.hendriks@student.nhlstenden.com>
2020-06-12 15:05:21 +0200

    Added Json and JS for changing the language

---
+++ js/lang.js
@@ -0,0 +1,43 @@
+var lang = findGetParameter("lang");
+if (lang == "nl") {
+    lang = "nl";
+} else {
+    lang = "en";
+}
+//document.getElementsByTagName("html").attr("lang") = lang;
+$.getJSON("../json/" + lang + ".json", function(data)  {
+    $.each(data, function(index, str) {
+        document.getElementById(index).innerHTML = str;
+    });
+});
+
+if (lang == "nl") {
+    document.getElementById("langA").href = "?lang=en";
+    document.getElementById("langImg").src = "assets/img/united-states.png";
+} else {
+    document.getElementById("langA").href = "?lang=nl";
+    document.getElementById("langImg").src = "assets/img/netherlands.png";
+}
+
+// var IDs = $("#page-top [id]").map(function() { return this.id; }).get();
+// var jsonObj = new Object();
+// $.each(IDs, function(index, str) {
+//     jsonObj[str] = document.getElementById(str).innerHTML;
+// });
+// console.log(jsonObj);
+
+
+//
+
+function findGetParameter(parameterName) {
+var result = null,
+    tmp = [];
+location.search
+    .substr(1)
+    .split("&")
+    .forEach(function (item) {
+      tmp = item.split("=");
+      if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+    });
+return result;
+}
